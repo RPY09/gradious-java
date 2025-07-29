d3dd11e9dd4e53be6ea6b6c75b11c33cdba84e19
Pranav-Reddamoni <153364659+RPY09@users.noreply.github.com>
2025-07-29 13:36:16 +0530

    unique number

--- Java/loops.java
+++ Java/loops.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class loops {
     static Scanner sc=new Scanner(System.in);
     public static void main(String args[]){
-       multi23();
+    //    multi23();
+        // countint();
+        System.out.println(unqiuenum());
     }
     static void multi23(){
         System.out.print("Enter range: ");
@@ -18,5 +20,40 @@ public class loops {
             i+=2;
             j+=3;
         }
+    }
+
+    static void countint(){
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        System.out.println("The count of digits are: "+count);
+    }
+    static boolean unqiuenum(){
+        System.out.print("Enter digits: ");
+        int n=sc.nextInt();
+        int c=n;
+        while(n>0){
+            int r=n%10;
+            int f=c;
+            int count=0;
+            while(f>0){
+                int r1=f%10;
+                if(r==r1){
+                    count++;
+                    if(count>1){
+                        // System.out.println("false");
+                        return false;
+                    }
+                }
+                f=f/10;
+            }
+            n=n/10;
+        }
+        // System.out.println("true");
+        return true;
     }
 }
