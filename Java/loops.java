5c56ecd2d1c854839bf214e111714fcd8b181d74
Pranav-Reddamoni <153364659+RPY09@users.noreply.github.com>
2025-07-29 14:53:08 +0530

    pascal triangle pattern

--- Java/loops.java
+++ Java/loops.java
@@ -105,13 +105,16 @@ public class loops {
         for(int i = 1; i <= n; i++){
             // Print spaces
             for(int j = 1; j <= n - i; j++){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             // Print numbers increasing
             for(int k = 1; k <= i; k++){
                 System.out.print(k + " ");
             }
-           
+            // Print numbers decreasing (optional for symmetry)
+            for(int k = i - 1; k >= 1; k--){
+                System.out.print(k + " ");
+            }
             System.out.println();
         }
     }
