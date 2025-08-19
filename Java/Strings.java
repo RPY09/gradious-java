0e34cceef4d6571acad437e090de24831bf18e47
Pranav-Reddamoni <153364659+RPY09@users.noreply.github.com>
2025-08-19 14:05:30 +0530

    trim the spaces to single space in the given string

--- Java/Strings.java
+++ Java/Strings.java
@@ -5,8 +5,10 @@ public class Strings {
     public static void main(String[] args) {
          System.out.print("Enter a String: ");
         String p=sc.nextLine();
+        System.out.println(p.length());
         // String result=reverse_String(p);
-        String result=word_reve_String(p);
+        // String result=word_reve_String(p);
+        String result=trim_Space(p);
         System.out.println(result);
     }
 //================================================================================REVERSE OF THE STRING=======================================
@@ -18,19 +20,44 @@ public class Strings {
         }
         return r;
     }
+//===================================================================================WORD SPACE REVERSE=======================================================================
     static String word_reve_String(String p){
         String result="";
        String word="";
        for(int i=p.length()-1;i>=0;i--){
         char ch=p.charAt(i);
+        
         if(ch==' '){
             result+=word+" ";
-            word="";        }
+            word="";        
+        }
         else{
             word=ch+word;
         }
        }
        result+=word;
         return result;
+    }
+//=============================================================================TRIM SPACES TO SINGLE SPACE======================================================================
+    static String trim_Space(String p){
+        String result="";
+        String word="";
+        for(int i=0;i<p.length();i++){
+            char ch=p.charAt(i);
+            
+            if(ch==' '){
+                if (p.charAt(i+1)!=' ') {   
+                    result+=word+" ";
+                    word="";        
+                }
+            }
+            else{
+                word=ch+word;
+            }
+        }
+        result+=word;
+        System.out.println(result.length());
+        return result;
+        
     }
 }
