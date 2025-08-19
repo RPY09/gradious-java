e32e430401a1241790e58dac4de5aaac8bf03bbf
Pranav-Reddamoni <153364659+RPY09@users.noreply.github.com>
2025-08-19 12:02:34 +0530

    word space reverse

--- Java/Strings.java
+++ Java/Strings.java
@@ -1,13 +1,36 @@
+import java.util.Scanner;
+
 public class Strings {
+    static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
-        reverse_String();
+         System.out.print("Enter a String: ");
+        String p=sc.nextLine();
+        // String result=reverse_String(p);
+        String result=word_reve_String(p);
+        System.out.println(result);
     }
-    static void reverse_String(){
-        String p="helllo";
+//================================================================================REVERSE OF THE STRING=======================================
+    static String reverse_String(String p){
+       
         String r="";
         for(int i=p.length()-1;i>=0;i--){
             r+=p.charAt(i);
         }
-        System.out.println(r);
+        return r;
+    }
+    static String word_reve_String(String p){
+        String result="";
+       String word="";
+       for(int i=p.length()-1;i>=0;i--){
+        char ch=p.charAt(i);
+        if(ch==' '){
+            result+=word+" ";
+            word="";        }
+        else{
+            word=ch+word;
+        }
+       }
+       result+=word;
+        return result;
     }
 }
