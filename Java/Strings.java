f8948681446790413484d2755cd32f0f204301d7
Pranav-Reddamoni <153364659+RPY09@users.noreply.github.com>
2025-08-26 13:57:21 +0530

    roman to integer and longest prefix

--- Java/Strings.java
+++ Java/Strings.java
@@ -4,17 +4,23 @@ public class Strings {
     static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         // String a="       aaaaaaaaaa   bb    ccc   ";
-         System.out.println("Enter a String: ");
-        String p=sc.nextLine();
+        //  System.out.println("Enter a String: ");
+        // String p="viii";
         // System.out.println(p.length());
         // String result=reverse_String(p);
         // String result=word_reve_String(p);
         // String result=trim_Space(p);
         // String result=add_num_String();
-        int  result=len_last_word(p);
-        System.out.println(result);
+
+        // int  result=len_last_word(p);
+        // System.out.println(result);
+        // anagram();
+        // System.out.println(romantoint(p));
+        String arr[]={"flower","flows","fls","flight"};
+        // System.out.println(arr[0].charAt(0));
+        System.out.println(longest_prefix(arr));
     }
-//================================================================================REVERSE OF THE STRING=======================================
+//================================================================================REVERSE OF THE STRING========================================================================
     static String reverse_String(String p){
        
         String r="";
@@ -81,6 +87,7 @@ public class Strings {
         System.out.println(sum);
         return null;
     }
+//===================================================================================LENGTH OF LAST WORD IN THE STRING==========================================================
     static int len_last_word(String p){
         String result="";
         p.trim();
@@ -89,8 +96,96 @@ public class Strings {
             if(ch==' '){
                 break;
             }
-            result+=(ch);
+            result+=ch;
         }   
         return result.length();
+    }
+    static void anagram(){
+        System.out.print("Enter First String");
+        String ana1=sc.nextLine().toLowerCase();
+        System.out.print("Enter second String");
+        String ana2=sc.nextLine().toLowerCase();
+        int count=0;
+        if(ana1.length()==ana2.length()){
+            for(int i=0;i<ana1.length();i++){
+                String result="";
+                char ch=ana1.charAt(i);
+                result+=ch;
+                if(ana2.contains(result)){
+                    ana2=ana2.replaceFirst(result,"");
+                    System.out.println(ana2);
+                    count++;
+                }
+            }
+            if(count==ana1.length()){
+                System.out.println("It is a anagram");
+            }
+            else{
+                System.out.println("Its not the anagram");
+            }
+        }
+        else{
+            System.out.println("length should be same for both strings..!");
+    }
+}
+    static int romantoint(String s){
+        s=s.toUpperCase();
+         int sum=0;
+        int num=0;
+        int count=0;
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(i<s.length()-1 && ch==s.charAt(i+1)){
+                count++;
+            }
+            
+        }
+        if(count<=2){
+            for(int i=0;i<s.length();i++){
+                char ch=s.charAt(i);
+                switch(ch){
+                    case 'I':
+                    num=1;
+                    break;
+
+                    case 'V':num=5;
+                    break;
+                    case 'X':num=10;
+                    break;
+                    case 'L':num=50;
+                    break;
+                    case 'C':num=100;
+                    break;
+                    case 'D':num=500;
+                    break;
+                    case 'M':num=1000;
+                    break;
+                }
+            if(i<s.length()-1 && num<romantoint(String.valueOf(s.charAt(i+1)))){
+                sum-=num;
+            }
+            else{
+                sum+=num;
+            }
+            }
+        }
+        else{
+            System.out.println("roman is wrong");
+        }
+        return sum;
+    }
+    static String longest_prefix(String[] arr){
+        String res="";
+        for(int i=0;i<arr.length;i++){
+            char ch=arr[0].charAt(i);
+            for(int j=1;j<arr.length;j++){
+                if(arr[j].charAt(i)!=ch){
+                    return res;
+                }
+            }
+            res+=ch;
+        }
+        return res;
     }
 }
