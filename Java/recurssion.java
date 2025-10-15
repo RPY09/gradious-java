f269abf9918dace7146426402469910c970975a7
Pranav-Reddamoni <153364659+RPY09@users.noreply.github.com>
2025-10-15 07:58:26 +0530

    reverse of an array using rescursion

--- Java/recurssion.java
+++ Java/recurssion.java
@@ -4,9 +4,17 @@ public class recurssion {
         // backTrackNumbers(1,9);
         // System.out.println(sumOfNaturalNum(5));
         // System.out.println(backtrackSumOfNatural(0,9));
-        System.out.println(factorial(9));
+        // System.out.println(factorial(9));
+        // System.out.println(backtrackFactorial(1,9));
+        int [] arr={1,2,3,4,5};
+        int res[]=reverseArray(arr,0,arr.length-1);
+        for(int num:res){
+            System.out.println(num+" ");
+        }
     }
+
     //call the function and decrese the i to traverse upto the condition and return then start printing the numbers in backtrack and the input is given as a largest number upto we want to print;
+
     static void numbersBacktrack(int i){
         if(i<1){
             return;
@@ -14,7 +22,9 @@ public class recurssion {
         numbersBacktrack(i-1);
         System.out.println(i);
     }
+
     //call the function and traverse upto the condition and return then start printing the elements in the reverse order this is backtracking;
+
     static void backTrackNumbers(int i,int n){
         if(i>n){
             return ;
@@ -22,22 +32,55 @@ public class recurssion {
         backTrackNumbers(i+1, n);
         System.out.println(i);
     }
+
+    //call the function and reduce the number till the 0 then return with adding the numbers from the 1-n;
+
     static int sumOfNaturalNum(int n){
         if(n==0){
             return 0;
         }
         return n+sumOfNaturalNum(n-1);
     }
+
+    //call the function and increse the number till the wanted number and add the numbers from the n-1;
+
     static int backtrackSumOfNatural(int i,int n){
         if(i==n){
             return n;
         }
         return i+backtrackSumOfNatural(i+1, n);
     }
+
+    //call the function and reduce the number till 1 and multiply the number from 1-n;
+
     static int factorial(int n){
         if(n==1 || n==0){
             return 1;
         }
         return n*factorial(n-1);
+    }
+
+    //call the function and add the number till n and multiply the number from n-1;
+
+    static int backtrackFactorial(int i,int n){
+        if(i==n){
+            return n;
+        }
+        return i*backtrackFactorial(i+1, n);
+    }
+
+    //
+
+    static int[] reverseArray(int[] arr,int i, int n){
+        if(i>=n){
+            return arr;
+        }
+        swap(arr,i,n);
+        return reverseArray(arr, i+1, n-1);
+    }
+    static void swap(int[] arr,int i,int n){
+        int t=arr[i];
+        arr[i]=arr[n];
+        arr[n]=t;
     }
 }
